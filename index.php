1fa9f684bdcd8f2dbda53fe20dfc82616a788754
nhut <nguyendongnhutqn1996@gmail.com>
2018-01-08 12:01:42 +0700

    chỉnh sửa file index trang bán hàng

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php 
     session_start();
-	if(isset($_GET["a"]) && $_GET["a"] != 0 && $_GET["a"] > 10)
-        $_SESSION["curURL"] = $_SERVER["REQUEST_URI"];
+    if(isset($_GET["a"]) )
+    {
+        if( $_GET["a"] != 0 && $_GET["a"] != 10 && $_GET["a"] != 11 )
+        {
+            $_SESSION["curURL"] = $_SERVER["REQUEST_URI"];
+        }
+    }     
+    else
+         $_SESSION["curURL"] = "index.php";
         
-    include_once("libs/class/DataProvider.php");
-    include_once("libs/class/Help.php");
+    include_once("./libs/class/DataProvider.php");
+    include_once("./libs/class/Help.php");
+    include_once("./libs/class/ShoppingCard.php");
+    include_once("./libs/class/TaiKhoan.php");
+    include_once("./libs/class/DatHang.php");
 ?>
 
 
@@ -12,33 +22,33 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php     include_once("./modules/mHeader.php"); ?>
 <title></title>
 </head>
 
 <body>
 <?php
-    include_once("modules/mHeader.php");
-    include_once("modules/mTopBar.php");
-    include_once("modules/mNavigation.php");
+    include_once("./modules/mTopBar.php");
+    include_once("./modules/mNavigation.php");
 ?>
 <div class="container">
     <div class="row" id="page">
         <div class="w25p pull-left">
             <?php
-            include("modules/mMenuLoai.php");
-            include("modules/mMenuHang.php");
+            include("./modules/mMenuLoai.php");
+            include("./modules/mMenuHang.php");
             ?>
         </div>
         <div class="w72p pull-right">
             <?php
-            include("modules/mContent.php");
+            include("./modules/mContent.php");
             ?>
         </div>
     </div> 
 </div>
 
 <?php
-    include("modules/mFooter.php");	
+    include("./modules/mFooter.php");	
 ?>
 </body>
 </html>
