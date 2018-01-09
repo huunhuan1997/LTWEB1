a88be40d932cd7944cf0efedaa92c714775b51a5
nhut <nguyendongnhutqn1996@gmail.com>
2018-01-09 10:44:30 +0700

    Xử Lý Đăng Nhập

--- libs/XuLy/DangNhap.php
+++ libs/XuLy/DangNhap.php
@@ -2,12 +2,12 @@
     if (isset($_POST['username']) && isset($_POST['password'])) 
     {
         $Username =$_POST['username'];
-        $Password = $_POST['password'];
+        $Password = md5($_POST['password']);
         $query = "SELECT MaTaiKhoan, TenHienThi, MaLoaiTaiKhoan
-                    FROM TaiKhoan
+                    FROM taikhoan
                     WHERE BiXoa = 0 AND TenDangNhap = '$Username'
                                     AND MatKhau = '$Password'";
-         $list = Provider::execQuery($query);
+         $list = Provider::execQuery($query);                                                                                                                   
          $row = mysqli_fetch_array($list);
          if($row!= null)
          {
@@ -16,10 +16,7 @@
              $_SESSION["MaLoaiTaiKhoan"] = $row["MaLoaiTaiKhoan"];
              $_SESSION['loginTime'] = time();
              $_SESSION['loggedIn'] = 1;
-             if (strrpos($url, "a=0") == false)
-                 provider::ChangeURL("index.php");
-            else
-                provider::ChangeURL($_SESSION["curURL"]);
+            provider::ChangeURL($_SESSION["curURL"]);
          }
          else
          {
